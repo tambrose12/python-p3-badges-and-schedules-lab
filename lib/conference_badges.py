2c08c6d970914aa0a773c84c456288e15807fcf2
Taylor Ambrose <taykesby@gmail.com>
2023-03-19 14:24:30 -0400

    lab complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    badge_list = []
+    for name in names:
+        badge_list.append(f"Hello, my name is {name}.")
+    return badge_list
+
 
 def assign_rooms(names):
-    return None
+    room_assignment = []
+    for index, name in enumerate(names):
+        room_assignment.append(
+            f"Hello, {name}! You'll be assigned to room {index + 1}!")
+    return room_assignment
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
